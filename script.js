925ae24384ddd553eeab06e758dd42deb7401ac9
Rômulo <Romulo>
2023-08-29 11:36:01 -0300

    my customization

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains("ligth")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-light2.png")
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar1.png")
   }
 }
